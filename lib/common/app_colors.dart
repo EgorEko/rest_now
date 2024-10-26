de1d6b7510426e900bdeaebad0c9709e82d98f1e
egorterekhov <asya.wear@gmail.com>
2024-10-26 15:14:33 +0300

    added app colors

---
+++ lib/common/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color activeSwitchColor = const Color.fromRGBO(30, 250, 70, 0.8);
+  static Color activeSwitchTextColor = const Color.fromRGBO(0, 177, 29, 1);
+  static Color backGroundColorStart = const Color.fromRGBO(228, 205, 243, 1.0);
+  static Color backGroundColorMiddle = const Color.fromRGBO(148, 122, 193, 1.0);
+  static Color backGroundColorEnd = const Color.fromRGBO(136, 106, 195, 1.0);
+  static Color inactiveSwitchColor = Colors.grey.withOpacity(0.3);
+  static Color appBarTextColor = Colors.deepPurple.withOpacity(0.6);
+  static Color white = Colors.white;
+}
